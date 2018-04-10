3c09633b018ca94a14d1d9d803dadab7df434845
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-09 17:41:30 -0700

    finish update ratings

--- ratings.py
+++ ratings.py
@@ -12,24 +12,37 @@ def ratings(filename):
         #     print "{} is rated at {}".format(restaurant, score)
 
     return rest_ratings
-            
 
-r = ratings("scores.txt")
-
-
+import random    
 
 def add_rating(rest_ratings):
-    rest_name = raw_input("Enter the name of the restaurant: ")
-    rating = int(raw_input("Enter the restaurant rating: "))
-    while rating > 5 or rating < 1:
-        rating = int(raw_input("Rating must be between 1 and 5: reenter the restaurant rating: "))
+    print "What would you like to do:"
+    choice = int(raw_input("Enter 1 to see all restaurant ratings, 2 to add a new restaurant or 3 to update a random restaurant or 4 to quit. "))
+    while choice != 4:
+        if choice == 1:
+            for restaurant, score in sorted(rest_ratings.iteritems()):
+                print "{} is rated at {}".format(restaurant, score)
+        elif choice == 2:
+            rest_name = raw_input("Enter the name of the restaurant: ")
+            rating = int(raw_input("Enter the restaurant rating: "))
+            while rating > 5 or rating < 1:
+                rating = int(raw_input("Rating must be between 1 and 5: reenter the restaurant rating: "))
+            rest_ratings[rest_name] = rating
+        elif choice == 3:
+               random_rest = random.choice(rest_ratings.keys())
+               new_rating = int(raw_input("Enter a new rating for {}: ".format(random_rest)))
+               rest_ratings[random_rest] = new_rating
+               print "Thanks for updating the rating!"
 
 
-    rest_ratings[rest_name] = rating
+                
+        print "What would you like to do:"
+        choice = int(raw_input("Enter 1 to see all restaurant ratings, 2 to add a new restaurant or 3 to quit. "))
+    return
 
-    for restaurant, score in sorted(rest_ratings.iteritems()):
-            print "{} is rated at {}".format(restaurant, score)
 
+
+r = ratings("scores.txt")
 add_rating(r)
             
 
